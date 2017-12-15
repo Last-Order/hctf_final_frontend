113ece138c3249718950206a46e6dea5caf5764f
Eridanus Sora <haolinaihui@Gmail.com>
2017-12-16 01:51:57 +0800

    remove vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex';
 import App from './App'
 import router from './router'
 
@@ -13,17 +12,6 @@ Vue.use(Vuex);
 Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
-
-const store = new Vuex.Store({
-  state: {
-    listening: false
-  },
-  mutations: {
-    startListening(state) {
-      state.listening = true;
-    }
-  }
-});
 
 /* eslint-disable no-new */
 new Vue({
