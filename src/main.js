23a76c11bd64e8b5bf0a85b8bb7ac963010e3f3a
Eridanus Sora <haolinaihui@Gmail.com>
2017-12-16 01:59:05 +0800

    remove vuex again

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css' // This line here
 
-Vue.use(Vuex);
 Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
@@ -17,7 +16,6 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
-  store,
   template: '<App/>',
   components: { App }
 })
